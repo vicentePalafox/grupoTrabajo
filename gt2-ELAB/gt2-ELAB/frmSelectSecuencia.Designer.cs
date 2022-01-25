25ddec3a5a153f4e850d08a432828c2fbbba0965
VicentePM <v.palafox.16@gmail.com>
2022-01-24 21:20:07 -0600

    Inicio de proyecto

---
+++ gt2-ELAB/gt2-ELAB/frmSelectSecuencia.Designer.cs
@@ -0,0 +1,112 @@
+﻿namespace gt2_ELAB
+{
+    partial class frmSelectSecuencia
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ComboSelect = new System.Windows.Forms.ComboBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnCerrar = new System.Windows.Forms.Button();
+            this.ComboAnalisis = new System.Windows.Forms.ComboBox();
+            this.btnComenzar = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // ComboSelect
+            // 
+            this.ComboSelect.FormattingEnabled = true;
+            this.ComboSelect.Location = new System.Drawing.Point(137, 77);
+            this.ComboSelect.Name = "ComboSelect";
+            this.ComboSelect.Size = new System.Drawing.Size(175, 21);
+            this.ComboSelect.TabIndex = 0;
+            this.ComboSelect.Text = "Seleccionar Practica";
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(175)))), ((int)(((byte)(39)))), ((int)(((byte)(44)))));
+            this.panel1.Controls.Add(this.btnCerrar);
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(456, 44);
+            this.panel1.TabIndex = 1;
+            // 
+            // btnCerrar
+            // 
+            this.btnCerrar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnCerrar.Location = new System.Drawing.Point(402, 0);
+            this.btnCerrar.Name = "btnCerrar";
+            this.btnCerrar.Size = new System.Drawing.Size(54, 44);
+            this.btnCerrar.TabIndex = 0;
+            this.btnCerrar.UseVisualStyleBackColor = true;
+            // 
+            // ComboAnalisis
+            // 
+            this.ComboAnalisis.FormattingEnabled = true;
+            this.ComboAnalisis.Location = new System.Drawing.Point(137, 139);
+            this.ComboAnalisis.Name = "ComboAnalisis";
+            this.ComboAnalisis.Size = new System.Drawing.Size(175, 21);
+            this.ComboAnalisis.TabIndex = 2;
+            this.ComboAnalisis.Text = "Selecciona Estacion de analisis";
+            // 
+            // btnComenzar
+            // 
+            this.btnComenzar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(174)))), ((int)(((byte)(24)))), ((int)(((byte)(48)))));
+            this.btnComenzar.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnComenzar.ForeColor = System.Drawing.Color.White;
+            this.btnComenzar.Location = new System.Drawing.Point(172, 193);
+            this.btnComenzar.Name = "btnComenzar";
+            this.btnComenzar.Size = new System.Drawing.Size(98, 30);
+            this.btnComenzar.TabIndex = 3;
+            this.btnComenzar.Text = "Comenzar";
+            this.btnComenzar.UseVisualStyleBackColor = false;
+            // 
+            // frmSelectSecuencia
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(455, 256);
+            this.Controls.Add(this.btnComenzar);
+            this.Controls.Add(this.ComboAnalisis);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.ComboSelect);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "frmSelectSecuencia";
+            this.Text = "frmSelectSecuencia";
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox ComboSelect;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btnCerrar;
+        private System.Windows.Forms.ComboBox ComboAnalisis;
+        private System.Windows.Forms.Button btnComenzar;
+    }
+}
